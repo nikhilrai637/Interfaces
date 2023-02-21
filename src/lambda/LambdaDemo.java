cf1c7b170a3bdec40ab586fe1b13b9dbb6f82d12
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-21 17:31:22 +0530

    using Consumer Interfaces

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -1,28 +1,13 @@
 package lambda;
 
+import java.util.List;
+
 public class LambdaDemo {
-    static String prefix = "-";
 
-    public LambdaDemo(String message) {
-        System.out.println(message + " from Constructor Method Reference");
-    }
-
-    public static void forMethodReference(String message){
-        System.out.println(message+" from Static Method Reference");
-    }
-    public  void forObjectMethodReference(String message){
-        System.out.println(message+" from Object Method Reference");
-    }
     public static void show(){
-         greet(System.out::println); // method reference of a static function
-         greet(LambdaDemo::forMethodReference);
-
-         var printer = new LambdaDemo("hmm");
-         greet(printer::forObjectMethodReference);
-         greet(LambdaDemo::new);
+       List<Integer> list = List.of(1,2,3);
+       list.forEach(number -> System.out.println(number*5));
     }
 
-    private static void greet(Printer printer) {
-        printer.print("Hello World!!");
-    }
+
 }
