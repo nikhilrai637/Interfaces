a6fd6dda5cf02e36f05f8e6cc008af6325ce00f9
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-23 19:16:39 +0530

    adding Consumer Interface - 2

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -8,6 +8,6 @@ public class LambdaDemo {
        List<String> list = List.of("a","b","c");
        Consumer<String> print = item -> System.out.println(item);
        Consumer<String> printUpperCase  = item->System.out.println(item.toUpperCase());
-       list.forEach(print.andThen(printUpperCase));
+       list.forEach(print.andThen(printUpperCase).andThen(print));
     }
 }
