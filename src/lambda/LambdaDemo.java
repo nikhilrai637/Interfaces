96ca4d9e9bebaf97d99904509f796ba1792b5938
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-23 19:30:12 +0530

    use case of Supplier Interface and Lazy Implementation

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -2,12 +2,12 @@ package lambda;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class LambdaDemo {
     public static void show(){
-       List<String> list = List.of("a","b","c");
-       Consumer<String> print = item -> System.out.println(item);
-       Consumer<String> printUpperCase  = item->System.out.println(item.toUpperCase());
-       list.forEach(print.andThen(printUpperCase).andThen(print));
+        Supplier<Double> random = ()->Math.random();
+        var number = random.get();
+        System.out.println(number);
     }
 }
