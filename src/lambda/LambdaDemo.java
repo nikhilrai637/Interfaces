146bab8156f414660fbd6c5bd911c1bef1057f94
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-25 23:20:35 +0530

    using UnaryOperators

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -5,13 +5,9 @@ import java.util.function.*;
 
 public class LambdaDemo {
     public static void show(){
-        BinaryOperator<Integer>add = (a,b)->a+b;
-        BinaryOperator<Integer>mul = (a,b)->a*b;
-        Function<Integer,Integer>square = (a)->a*a;
-        var result = add.andThen(square).apply(1,2);
-        var result2 = mul.andThen(square).apply(3,4);
-        System.out.println(result2);
+        UnaryOperator<Integer> square = n -> n*n;
+        UnaryOperator<Integer> increment = n -> n+1;
+        var result = increment.andThen(square).apply(1);
         System.out.println(result);
-
     }
 }
