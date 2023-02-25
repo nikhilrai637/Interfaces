3a3a574ee45e6c60926c6a8fede02ce96fc08794
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-25 18:59:16 +0530

    adding Predicates

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -3,23 +3,14 @@ package lambda;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class LambdaDemo {
     public static void show(){
-         //key : value
-        //first : "key = value"
-        // second : "{key = value}"
-
-        Function<String,String>replaceColon = str->str.replace(':','=');
-        Function<String,String>braces       = str-> "{" + str + "}";
-        var result = replaceColon
-                    .andThen(braces)
-                    .apply("key:value");
-
-        result = braces.compose(replaceColon).apply("key:value");
+        //Predicate
+        Predicate<String> isLongerThan5 = str -> str.length() > 5;
+        var result = isLongerThan5.test("A string");
         System.out.println(result);
-
-
     }
 }
