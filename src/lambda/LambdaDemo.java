a278fca941bbb9b78f3ef2c589288e099847d860
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-25 16:32:14 +0530

    using Functional Interface

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -2,12 +2,13 @@ package lambda;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class LambdaDemo {
     public static void show(){
-        Supplier<Double> random = ()->Math.random();
-        var number = random.get();
-        System.out.println(number);
+        Function<String,Integer> map = str->str.length();
+        var length = map.apply("Rider");
+        System.out.println(length);
     }
 }
