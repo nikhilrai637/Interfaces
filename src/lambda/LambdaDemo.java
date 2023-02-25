fcad6d7025761c8037aa6e1f8150a2652cbff58e
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-25 23:05:15 +0530

    Binary Operators Combining

--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -1,19 +1,17 @@
 package lambda;
 
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class LambdaDemo {
     public static void show(){
-        //Predicate
-        Predicate<String>hasLeftBrace  = str -> str.startsWith("{");
-        Predicate<String>hasRightBrace = str -> str.endsWith("}");
-        Predicate<String> hasLeftAndRightBrace = hasLeftBrace.and(hasRightBrace);
-        var result = hasLeftAndRightBrace.test("{key : value}");
+        BinaryOperator<Integer>add = (a,b)->a+b;
+        BinaryOperator<Integer>mul = (a,b)->a*b;
+        Function<Integer,Integer>square = (a)->a*a;
+        var result = add.andThen(square).apply(1,2);
+        var result2 = mul.andThen(square).apply(3,4);
+        System.out.println(result2);
         System.out.println(result);
-        System.out.println(hasLeftBrace.or(hasRightBrace).test("{key : pair}"));
+
     }
 }
